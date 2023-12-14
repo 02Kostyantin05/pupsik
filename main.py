72472b3ae21fc6f0adb264ac6580378679308cbf
02Kostyantin05 <152997320+02Kostyantin05@users.noreply.github.com>
2023-12-14 16:54:51 +0200

    чь
    л

--- main.py
+++ main.py
@@ -1,9 +1,79 @@
-def parne_ili_net(nomer):
-    return nomer % 2 == 0
-user_input = input("Введіть число: ")
-nomer_to_check = int(user_input)
+class Human:
+    def __init__(self, name , othername):
+        self.name = "oleg"
+        self.othername = "margo gosling"
+        self.money = 100
+        self.pleasure = 50
+        self.health = 100
 
-if parne_ili_net(nomer_to_check):
-    print(f"{nomer_to_check} є парним числом.")
-else:
-    print(f"{nomer_to_check} є непарним числом.")
+
+    def relax(self, hours):
+        self.pleasure += hours * 10
+
+        print(f"{self.name} на чиле на раслабоне.")
+
+    def work(self, hours):
+        income = hours * 20
+        self.money += income
+        print(f"{self.name} на заводе {hours} часов зато {income} мани на кармане.")
+
+    def shopping(self, items_cost):
+        if self.money >= items_cost:
+            self.money -= items_cost
+            self.pleasure += 30
+            print(f"{self.name}пашол и купил ядерку.")
+        else:
+            print(f"{self.name} бедній бомжара.")
+
+    def house(self):
+        print(f"{self.name}дома не работая.")
+
+    def relationship(self, other):
+        self.pleasure += 30
+        print(f"{self.name} занимається личнай жизню с {other.name}.")
+
+    def life(self):
+        self.health -= 5
+        self.pleasure -= 5
+        print(f"{self.name} вииживает.")
+        self.money -= 20
+        self.food +- 10
+
+
+    def is_alive(self):
+        return self.health > 0
+
+    def info(self):
+        print(f"Name: {self.name}, Money: {self.money}, Pleasure: {self.pleasure}, Health: {self.health}")
+
+class Car:
+    def __init__(self, marka):
+        self.marka = marka
+        self.passengers = []
+
+    def add_passenger(self, human):
+        self.passengers.append(human)
+
+    def passengers_info(self):
+        if self.passengers:
+            print(f"в машині  {self.marka}, пасажирів: {', '.join(p.name for p in self.passengers)}")
+        else:
+            print("немає пасажирів.")
+
+class Home:
+    def __init__(self):
+        self.food = 0
+        self.cleanliness_level = 100
+
+    def eat(self, human):
+        if self.food > 0:
+            self.food -= 1
+            human.health += 10
+            print(f"{human.name} наелса і спит.")
+        else:
+            print(f"нету шо кушать ((( {human.name}.")
+
+    def clean(self):
+        self.cleanliness_level = 100
+        print("типерь чиста и красива.")
+
