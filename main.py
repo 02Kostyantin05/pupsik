6194a2f11d7994dc1372e87ee85dfeff011a0fc3
02Kostyantin05 <152997320+02Kostyantin05@users.noreply.github.com>
2023-12-14 11:35:17 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('netfluks')
+def parne_ili_net(nomer):
+    return nomer % 2 == 0
+user_input = input("Введіть число: ")
+nomer_to_check = int(user_input)
+
+if parne_ili_net(nomer_to_check):
+    print(f"{nomer_to_check} є парним числом.")
+else:
+    print(f"{nomer_to_check} є непарним числом.")
